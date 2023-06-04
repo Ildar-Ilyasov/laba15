ff239307910b660fa4edaea3320d27b61dc2b241
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-06-04 21:02:29 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,33 @@
 import java.io.*;
 import java.io.ObjectOutputStream;
+import java.util.Objects;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Для записи состояния объекта введите save ");
-        System.out.println("Для записи состояния объекта введите upload");
         while(true) {
-            String vvod = in.next();
-            switch (vvod) {
-                case "save" -> {
-                    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("result.txt"))) {
-                        System.out.println("Введите x");
-                        int x = in.nextInt();
-                        Calculation calc = new Calculation(x);
-                        calc.action();
-                        oos.writeObject(calc);
-                    }
-                    catch (IOException ex) {
-                        System.out.println(ex.getMessage());
-                    }
+            String coms = in.next();
+            if (Objects.equals(coms, "save")){
+                try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("result.txt"))) {
+                    function urav = new function(in.nextInt());
+                    urav.count();
+                    oos.writeObject(urav);
                 }
-                case "upload" -> {
-                    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("result.txt"))) {
-                        Calculation calc = (Calculation)ois.readObject();
-                        System.out.println("x=" + calc.x + " y=" + calc.y);
-                    } catch (Exception ex) {
-                        System.out.println(ex.getMessage());
-                    }
+                catch (IOException ex) {
+                    System.out.println(ex.getMessage());
                 }
-                case "exit" -> {
-                    return;
+
+            }
+            else if (Objects.equals(coms, "upload")){
+                try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("result.txt"))) {
+                    function calc = (function)ois.readObject();
+                    System.out.println("y=" + calc.y);
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
                 }
-                default -> System.out.println("не знаю такой команды");
+            }
+            else{
+                System.out.println("такой команды нет");
             }
         }
     }
